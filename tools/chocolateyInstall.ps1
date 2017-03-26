4890a1b8d25e2d444ecf03650071aeb2f475845e
Zed <dev@zedcn.com>
2017-03-26 22:02:14 +0800

    Update Version to 1.0.5

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
-$packageName = 'Webstorm'
+$packageName = 'tim'
 $installerType = 'EXE' 
-$url = 'https://download.jetbrains.com/webstorm/WebStorm-2017.1.exe'
+$url = 'http://dldir1.qq.com/qqfile/qq/TIM1.0.5/20303/TIM1.0.5.exe'
 $url64 = $url 
-$silentArgs = '/S' 
+$silentArgs = '-s' 
 $validExitCodes = @(0) 
 $checksumType = 'sha256';
-$checksum = '9a3f7a8ce59a4183d6a75470bc17624240e1d5a88c97df39233ce5462a0bd5e7';
+$checksum = '718249DA46A04C1E2C8E2A547C91D1510A99CBBD3759E7B295A3D4EBC53866FE';
 
 Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64  -validExitCodes $validExitCodes -Checksum64 $checksum -ChecksumType64 $checksumType -ChecksumType "$checksumType" -Checksum $checksum
--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -1,27 +1,25 @@
-$packageName = 'Webstorm'
+$packageName = 'tim'
 
 $extractionPath = (${env:ProgramFiles(x86)}, ${env:ProgramFiles} -ne $null)[0]
 
-$installDir = Join-Path $extractionPath 'JetBrains'
+$installDir = Join-Path $extractionPath 'Tencent\TIM'
 
-$installVersionDir  = Join-Path $installDir  'WebStorm 2017.1'
-
-if (Test-Path ("${installVersionDir}\bin\Uninstall.exe")) {
-	$uninstallExe = (gci "${installVersionDir}\bin\Uninstall.exe").FullName | sort -Descending | Select -first 1
+if (Test-Path ("${installDir}\TIMUninst.exee")) {
+	$uninstallExe = (gci "${installDir}\TIMUninst.exe").FullName | sort -Descending | Select -first 1
 
 	$params = @{
 		PackageName = $packageName;
 		FileType = "exe";
-		SilentArgs = "/S";
+		SilentArgs = "-s";
 		File = $uninstallExe;
 	}   
 
 	Uninstall-ChocolateyPackage @params
 
-	if (Test-Path ("${installVersionDir}\bin\Uninstall.exe")) {		
-		$directoryInfo = Get-ChildItem $installVersionDir | Measure-Object
+	if (Test-Path ("${installDir}\TIMUninst.exe")) {		
+		$directoryInfo = Get-ChildItem $installDir | Measure-Object
     	if ($directoryInfo.count -eq 1) {
-      		Remove-Item "${installVersionDir}" -recurse -force -confirm:$false
+      		Remove-Item "${installDir}" -recurse -force -confirm:$false
     	}
 	}
 }

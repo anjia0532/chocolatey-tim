7f4d7d040a10681b76c306813967fca77107a7ad
zed <dev@zedcn.com>
2017-06-16 21:35:03 +0800

    Update to 1.1.5

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 $packageName = 'tim'
 $installerType = 'EXE' 
-$url = 'http://dldir1.qq.com/qqfile/qq/TIM1.1.0/20837/TIM1.1.0.exe'
+$url = 'http://dldir1.qq.com/qqfile/qq/TIM1.1.5/21175/TIM1.1.5.exe'
 $url64 = $url 
 $silentArgs = '-s' 
 $validExitCodes = @(0) 
 $checksumType = 'sha256';
-$checksum = '8862E362DB810F7AA7458AD7051B6ACE842D96DEE267C883812008B1C8640417';
+$checksum = 'e9b61f6ddd6be59cc39528d961103dfac826e7cc94975f76555bbb75ff72b7a3';
 
 Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64  -validExitCodes $validExitCodes -Checksum64 $checksum -ChecksumType64 $checksumType -ChecksumType "$checksumType" -Checksum $checksum

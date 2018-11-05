29de9498a107eccc8699b2b217fd1b2fb0c15bd4
Zed <dev@zedcn.com>
2018-11-05 12:57:48 +0800

    update to 2.3.0.20979

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 $packageName = 'tim'
 $installerType = 'EXE' 
-$url = 'https://qd.myapp.com/myapp/qqteam/tim/down/tim_pc.exe'
+$url = 'https://dldir1.qq.com/qqfile/qq/PCTIM2.3.0/20979/TIM2.3.0.20979.exe'
 $url64 = $url 
 $silentArgs = '-s' 
 $validExitCodes = @(0) 
 $checksumType = 'sha256';
-$checksum = 'df5c8880e4d6c500aab5a12bdf4ecbc6fa05f505e54267b7bfeb02acc92e19be';
+$checksum = '7c2cfbacba6467ff933aa157070827d99ee3a964d8ba31f156c9d2b16fed87f4';
 
 Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64  -validExitCodes $validExitCodes -Checksum64 $checksum -ChecksumType64 $checksumType -ChecksumType "$checksumType" -Checksum $checksum

1ca58b375e65c56449bc5028208d11849a737e6e
Zed <dev@zedcn.com>
2019-03-02 22:36:56 +0800

    2.3.2

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 $packageName = 'tim'
 $installerType = 'EXE' 
-$url = 'https://dldir1.qq.com/qqfile/qq/PCTIM2.3.1/20994/TIM2.3.1.20994.exe'
+$url = 'https://dldir1.qq.com/qqfile/qq/PCTIM2.3.2/21158/TIM2.3.2.21158.exe'
 $url64 = $url 
 $silentArgs = '-s' 
 $validExitCodes = @(0) 
 $checksumType = 'sha256';
-$checksum = 'D40C23F467D080D964DBFA766DDED96815E9CD57C87CAA0A9413B6D62019C9F6';
+$checksum = '212F45D12FA9D705D4CAA99D6A674A42F3818D83D364F7E5C1C70028E3A0E77F';
 
 Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64  -validExitCodes $validExitCodes -Checksum64 $checksum -ChecksumType64 $checksumType -ChecksumType "$checksumType" -Checksum $checksum

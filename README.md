c7cfd5ef1608402142b9b35933e4cf3bab261cc6
Zed <dev@zedd.dev>
2019-10-02 10:04:31 +0800

    bump verion; add github actions;

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Chocolatey-TIM
 
-[![Build status](https://ci.appveyor.com/api/projects/status/aildo91u0g1elej5?svg=true)](https://ci.appveyor.com/project/ZeddG93/chocolatey-tim)
+[](https://github.com/ZeddG93/chocolatey-tim/workflows/workflow.yml/badge.svg)
--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 $packageName = 'tim'
 $installerType = 'EXE' 
-$url = 'https://dldir1.qq.com/qqfile/qq/PCTIM2.3.2/21158/TIM2.3.2.21158.exe'
+$url = 'https://dldir1.qq.com/qqfile/qq/TIM2.3.2/21173/TIM2.3.2.21173.exe'
 $url64 = $url 
 $silentArgs = '-s' 
 $validExitCodes = @(0) 
 $checksumType = 'sha256';
-$checksum = '212F45D12FA9D705D4CAA99D6A674A42F3818D83D364F7E5C1C70028E3A0E77F';
+$checksum = '0fafdc98d7fa2c9d8b07fdcf327c462ff19e5fa38333a19892745de9c64186d9';
 
 Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64  -validExitCodes $validExitCodes -Checksum64 $checksum -ChecksumType64 $checksumType -ChecksumType "$checksumType" -Checksum $checksum

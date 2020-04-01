00740cb436c97690e8f37768d51c9955f717a128
Zed <zed@zedd.dev>
2020-04-01 09:55:24 +0800

    upgrade to 3.0.0.21315

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 $packageName = 'tim'
 $installerType = 'EXE' 
-$url = 'https://dldir1.qq.com/qqfile/qq/TIM2.3.2/21173/TIM2.3.2.21173.exe'
+$url = 'https://dldir1.qq.com/qqfile/qq/TIM3.0.0/TIM3.0.0.21315.exe'
 $url64 = $url 
 $silentArgs = '-s' 
 $validExitCodes = @(0) 
 $checksumType = 'sha256';
-$checksum = '0fafdc98d7fa2c9d8b07fdcf327c462ff19e5fa38333a19892745de9c64186d9';
+$checksum = '275995dd686750697718cdcf4413588c1d2ad6ef49e58f1754ffe92e069889bb';
 
 Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64  -validExitCodes $validExitCodes -Checksum64 $checksum -ChecksumType64 $checksumType -ChecksumType "$checksumType" -Checksum $checksum

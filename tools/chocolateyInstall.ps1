84e0b5555db70779628bf95cec2005dd6ac2d5bf
AnJia <anjia0532@gmail.com>
2021-05-18 15:42:25 +0800

    upgrade to TIM3.3.5.22018.exe

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 $packageName = 'tim'
 $installerType = 'EXE' 
-$url = 'https://dldir1.qq.com/qqfile/qq/TIM3.0.0/TIM3.0.0.21315.exe'
+$url = 'https://dldir1.qq.com/qqfile/qq/PCTIM/TIM3.3.5/TIM3.3.5.22018.exe'
 $url64 = $url 
 $silentArgs = '-s' 
 $validExitCodes = @(0) 
 $checksumType = 'sha256';
-$checksum = '275995dd686750697718cdcf4413588c1d2ad6ef49e58f1754ffe92e069889bb';
+$checksum = 'D7D815D93C53047282D8055A5FBE1ED589AD90F64ED7CD57BBB5364E6DDF06F9';
 
 Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64  -validExitCodes $validExitCodes -Checksum64 $checksum -ChecksumType64 $checksumType -ChecksumType "$checksumType" -Checksum $checksum
